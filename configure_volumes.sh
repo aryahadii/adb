4254d5c5e51eba0754a617eed7efa1be493bd5d5
erfan.mehraban <erfan.mehraban@gmail.com>
2018-11-18 16:43:27 +0330

    add bash script for automate create dirs

---
+++ configure_volumes.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+mkdir /mnt/storage/$@/data
+mkdir /mnt/storage/$@/data/postgres
+mkdir /mnt/storage/$@/data/cockroach
+mkdir /mnt/storage/$@/data/hdfs
+mkdir /mnt/storage/$@/data/hdfs/historyserver
+mkdir /mnt/storage/$@/data/hdfs/datanode
+mkdir /mnt/storage/$@/data/hdfs/nodename
+sed -i -e "s/HOME/$@/g" ./cockroachDB/docker-compose.yml
+sed -i -e "s/HOME/$@/g" ./flink-spark-hdfs/docker-compose.yml
+sed -i -e "s/HOME/$@/g" ./postgres/docker-compose.yml
